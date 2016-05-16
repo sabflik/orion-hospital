9bf9346bc789c1817da24f1d7a1f4be95942b29b
sabflik <szaf045@aucklanduni.ac.nz>
2016-05-17 10:41:33 +1200

    Created Patients class

---
+++ Patients.java
@@ -0,0 +1,19 @@
+public class Patient {
+	
+	private int id;
+	private String name;
+	private String dob;
+	private int gender;
+
+	Patient(int id, String name, String dob, int gender) {
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+		this.gender = gender;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
