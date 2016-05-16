5b17a74cea904e9aeefd02ccea002e456152faaf
sabflik <szaf045@aucklanduni.ac.nz>
2016-05-17 11:22:33 +1200

    Create Ward.java

---
+++ Ward.java
@@ -0,0 +1,25 @@
+public class Ward {
+	
+	private int id;
+	private String name;
+	private int capacity;
+
+	Ward(int id, String name, int capacity) {
+		this.id = id;
+		this.name = name;
+		this.capacity = capacity;
+	}
+	
+	int getId() {
+		return id;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	int getcapacity() {
+		return capacity;
+	}
+	
+}
